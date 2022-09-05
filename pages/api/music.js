3c4e98aa04bc4c4ef4131cd60aa3c31770e6d902
Fabrice Geib <fabricegeib@gmail.com>
2022-09-05 22:11:58 +0200

    Create music.js

---
+++ pages/api/music.js
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+	res.status(200).json({
+		count: 3,
+		next: "/api/music/?page=2",
+		previous: null,
+		results: [
+			{
+				title: "You stay on my mind",
+			},
+			{
+				name: "Pierre",
+			},
+			{
+				name: "Sam",
+				height: "155",
+				mass: "50",
+				hair_color: "n/a",
+				skin_color: "fair",
+				eye_color: "brown",
+				birth_year: "112BBY",
+				gender: "female",
+				homeworld: "https://diversunivers.vercel.app/api/planets/1/",
+				films: [
+					"https://diversunivers.vercel.app/api/films/1/",
+					"https://diversunivers.vercel.app/api/films/2/",
+					"https://diversunivers.vercel.app/api/films/3/",
+					"https://diversunivers.vercel.app/api/films/6/",
+				],
+				species: ["https://diversunivers.vercel.app/api/species/2/"],
+				vehicles: [],
+				starships: [],
+				created: "20022-09-04T19:01:23.357000Z",
+				edited: "2022-09-04T19:16:42.309000Z",
+				url: "https://diversunivers.vercel.app/api/people/2/",
+			},
+		],
+	});
+}

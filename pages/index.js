3438f842ca635d7289515025f8213200740191b6
Fabrice Geib <fabricegeib@gmail.com>
2022-12-06 02:05:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,20 +52,20 @@ export default function Home() {
 						<p>Find in-depth information about Next.js features and API.</p>
 					</a>
 
-					<a href="https://nextjs.org/learn" className={styles.card}>
+					{/* <a href="https://nextjs.org/learn" className={styles.card}>
 						<h3>Learn &rarr;</h3>
 						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
+					</a> */}
 
-					<a
+					{/* <a
 						href="https://github.com/vercel/next.js/tree/master/examples"
 						className={styles.card}
 					>
 						<h3>Examples &rarr;</h3>
 						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
+					</a> */}
 
-					<a
+					{/* <a
 						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 						className={styles.card}
 					>
@@ -73,7 +73,7 @@ export default function Home() {
 						<p>
 							Instantly deploy your Next.js site to a public URL with Vercel.
 						</p>
-					</a>
+					</a> */}
 				</div>
 			</main>
 
